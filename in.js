f7d176fcb39f56da16903d48452102bbc23f4f7d
Stan1812 <2314980309@QQ.com>
2017-05-27 15:33:13 +0800

    增加了图片处理：高斯模糊

--- in.js
+++ in.js
@@ -5,18 +5,26 @@ var ctx=canvas.getContext('2d');
 var addtext=document.getElementById('addtext')
 
 
-//canvas尺寸调整
+
+//canvas辣鸡尺寸调整，发现在基于canvas的时候拖动貌似不是很好解决
 function cvs_size_(){
-  
+	
 function cvs_size_chag(event){
 
-    var w_x=event.layerX;
-    var h_y=event.layerY;
-    canvas.height=h_y;
-    canvas.width=w_x;
+		var w_x=event.layerX;
+		var h_y=event.layerY;
+		canvas.height=h_y;
+		canvas.width=w_x;
 
-}
-canvas.addEventListener('click',cvs_size_chag);
+}  
+		var i=0;
+		
+		canvas.addEventListener('click',function(){
+	
+		if (i==0) 
+		{cvs_size_chag(event);i=1}
+
+});
 }
 
 var cvs_size_btn=document.createElement('div');
@@ -24,26 +32,23 @@ cvs_size_btn.appendChild(document.createTextNode('>'));
 cvs_size_btn.style.cssText="width:20px; height:20px;background:red;float:right"
  document.getElementById('item1').appendChild(cvs_size_btn);
 
-function change_(){
-    cvs_size_();
-    cvs_size_btn.removeEventListener('click',change_,true)
-   
-}
-cvs_size_btn.addEventListener('click',change_,true)
+
+cvs_size_btn.addEventListener('click',cvs_size_);
+
 
 
 //展示和隐藏切换
-  
+	
 
 function toggle(targetid){
-    if (document.getElementById){  
-      console.log(targetid.style.display)            
-            if (targetid.style.display==""){
-                targetid.style.display="none";
-            } else {
-                targetid.style.display="";
-            }
-    }
+		if (document.getElementById){  
+			console.log(targetid.style.display)            
+						if (targetid.style.display==""){
+								targetid.style.display="none";
+						} else {
+								targetid.style.display="";
+						}
+		}
 }
 
 
@@ -70,11 +75,11 @@ ctx2.fillRect(0,0,300,300);
 
 /* 更加精确的，，但是现在写的有点晕。。不知道怎么算。。心态崩了
 for (var i=0;i<300;i++){
-    for (var j=0;j<300;j++){
-      ctx2.fillStyle = 'rgb(' + Math.floor(255-42.5*i) + ',' + 
-                       Math.floor(255-42.5*j) + ',0)';
-      ctx2.fillRect(j*1,i*1,1,1);
-    }
+		for (var j=0;j<300;j++){
+			ctx2.fillStyle = 'rgb(' + Math.floor(255-42.5*i) + ',' + 
+											 Math.floor(255-42.5*j) + ',0)';
+			ctx2.fillRect(j*1,i*1,1,1);
+		}
 }
 */
 
@@ -82,16 +87,16 @@ for (var i=0;i<300;i++){
 var color = document.getElementById('color');
 
 function pick(event) {
-  var x = event.offsetX;
-  var y = event.offsetY;
-  var pixel = ctx2.getImageData(x, y, 1, 1);
-  var data = pixel.data;
-  var rgba = 'rgba(' + data[0] + ',' + data[1] + ',' + data[2] + ',' + (data[3] / 255) + ')';
-  color.style.background = rgba;
-  color.textContent = rgba;
-  console.log(rgba,x,y)
-  ctx.fillStyle=rgba;
-  ctx.strokeStyle=rgba;
+	var x = event.offsetX;
+	var y = event.offsetY;
+	var pixel = ctx2.getImageData(x, y, 1, 1);
+	var data = pixel.data;
+	var rgba = 'rgba(' + data[0] + ',' + data[1] + ',' + data[2] + ',' + (data[3] / 255) + ')';
+	color.style.background = rgba;
+	color.textContent = rgba;
+	console.log(rgba,x,y)
+	ctx.fillStyle=rgba;
+	ctx.strokeStyle=rgba;
 
 }
 canvas2.addEventListener('click', pick);
@@ -148,81 +153,91 @@ var scal_control=document.getElementById('_scale').value;
 var pic1=document.getElementById('uppic').value;
 console.log(pic1)
 
-  function readAsDataURL(){
-    //检验是否为图像文件
-   	document.getElementById('read_pic_but').className+='button'
-    var file = document.getElementById("uppic").files[0];
-    if(!/image\/\w+/.test(file.type)){
-        alert('仅支持上传图片');
-        return false;
-    }
-    var reader = new FileReader();
-    //将文件以Data URL形式读入页面
-    reader.readAsDataURL(file);
-    reader.onload=function(e){
-        var picture=this.result;
-       	var pic=new Image();
-       	pic.src=picture;
-       	//调整图片的大小
-       	var scal_control=document.getElementById('_scale').value; 
+	function readAsDataURL(){
+		//检验是否为图像文件
+		document.getElementById('read_pic_but').className+='button'
+		var file = document.getElementById("uppic").files[0];
+		if(!/image\/\w+/.test(file.type)){
+				alert('仅支持上传图片');
+				return false;
+		}
+		var reader = new FileReader();
+		//将文件以Data URL形式读入页面
+		reader.readAsDataURL(file);
+		reader.onload=function(e){
+				var picture=this.result;
+				var pic=new Image();
+				pic.src=picture;
+				//调整图片的大小
+				var scal_control=document.getElementById('_scale').value; 
 
-        if (scal_control!="") 
-       	{ ctx.scale(scal_control,scal_control);}
-   		if (scal_control="")
-   		{
-      		scal_control= 1;
-      		ctx.scale(scal_control,scal_control);     		
-      	};
-        
-       	console.log(scal_control);
-       	ctx.drawImage(pic,0,0)
-      	
+				if (scal_control!="") 
+				{ ctx.scale(scal_control,scal_control);}
+			if (scal_control="")
+			{
+					scal_control= 1;
+					ctx.scale(scal_control,scal_control);     		
+				};
+				
+				console.log(scal_control);
+				ctx.drawImage(pic,0,0)
+				
 
-    }
+		}
 }
 
 //图片处理的其他功能
 
-//滤镜，黑白，负片
+//图片处理中的各种滤镜
 
 function pix_procs(){
-  var imageData0 = ctx.getImageData(0, 0, canvas.width,canvas.height);
-  var data0 = imageData0.data;
-    
- 	
-	function  grayscale()  {
-    	for (var i = 0; i < data0.length; i += 4) {
-     	 var avg = (data0[i] + data0[i + 1] + data0[i + 2]) / 3;
-      	data0[i]     = avg; // red
-      	data0[i + 1] = avg; // green
-      	data0[i + 2] = avg; // blue
-    	}
-    	ctx.putImageData(imageData0, 0, 0);
-    	console.log(imageData0);
-    };
 
+	var imageData0 = ctx.getImageData(0, 0, canvas.width,canvas.height);
+	var data0 = imageData0.data;
+	data_length=data0.length;
+	//黑白效果
+	function grayscale()  {
+			for (var i = 0; i < data_length; i += 4) {
+			 var avg = (data0[i] + data0[i + 1] + data0[i + 2]) / 3;
+				data0[i]     = avg; // red
+				data0[i + 1] = avg; // green
+				data0[i + 2] = avg; // blue
+			}
+			ctx.putImageData(imageData0, 0, 0);
+			console.log(imageData0);
+		};
 
-    function negative(){
-    	for(var j=0;j<data0.length;j+=4){
-    		data0[j]=255-data0[j];
-    		data0[j+1]=255-data0[j+1];
-    		data0[j+2]=255-data0[j+2];
-    	}
-    	ctx.putImageData(imageData0, 0, 0);
-    	console.log(imageData0);
-    }
+	//负片效果
+	function negative(){
+		for(var j=0;j<data_length;j+=4){
+			data0[j]=255-data0[j];
+			data0[j+1]=255-data0[j+1];
+			data0[j+2]=255-data0[j+2];
+		}
+		ctx.putImageData(imageData0, 0, 0);
+		console.log(imageData0);
+	}
 	
-
+	//高斯模糊
+	function gauss_blur_() {
+		stackBlurCanvasRGBA('canvas',0,0, 1000, 500, 3 );
+	}
 
  var grayscalebtn = document.getElementById('grayscalebtn');
  grayscalebtn.addEventListener('click',grayscale);
+
  var negativebtn = document.getElementById('negative');
  negativebtn.addEventListener('click',negative);
- console.log("im clicked")
+
+ var gauss_blur_btn=document.getElementById('gauss_blur');
+ gauss_blur_btn.addEventListener('click',gauss_blur_)
+  console.log("im clicked")
 }
 
 var pix_procsbtn=document.getElementById('pix_procs');
 pix_procsbtn.addEventListener('click',pix_procs);
+
+
 //打马赛克
 
 function mosaic_drawer_ctol(){
@@ -232,15 +247,20 @@ function mosaic_drawer_ctol(){
 		var layer_y=event.offsetY;4
 		var pix = ctx.getImageData(0,0,10,10);
 		console.log(pix);
-  		ctx.putImageData(pix,layer_x,layer_y);
+			ctx.putImageData(pix,layer_x,layer_y);
 	}
-	canvas.addEventListener('click',mosaic_drawer);}
+		canvas.addEventListener('click',function(){
+			var i=0;
+			if (i==0) { 
+			mosaic_drawer(event);
+			i=1;}
+		});}
 document.getElementById('mosaic_drawer').addEventListener('click',mosaic_drawer_ctol)
 
 //手写笔迹
 
  /*function line_writer (){
-               
+							 
 canvas.addEventListener('touchstart',function(evt){
 evt.preventDefault();
 context.beginPath();
@@ -260,7 +280,7 @@ line_writer();*/
  var linex = new Array();  
  var liney = new Array();  
  var linen = new Array();  
-  
+	
  var lastX = -1;  
  var lastY = -1;  
 
@@ -275,38 +295,38 @@ canvas.addEventListener('mousemove', onMouseMove, false);
  canvas.addEventListener('mouseup', onMouseUp, false);  
  
 
-  
+	
  function onMouseMove(evt) {  
-    if (flag == 1) {  
-       linex.push(evt.layerX);  
-       liney.push(evt.layerY);  
-       linen.push(1);  
-       ctx.beginPath();  
-       ctx.lineWidth = 5 + Math.random() * 10;  
-  
-       for (var i=1;i<linex.length;i++) {  
-             lastX = linex[i];  
-             lastY = liney[i];  
-             if (linen[i] == 0) {  
-                ctx.moveTo(lastX,lastY);  
-             } else {  
-                ctx.lineTo(lastX,lastY);  
-             }  
-       } 
-       ctx.stroke();
-    }  
+		if (flag == 1) {  
+			 linex.push(evt.layerX);  
+			 liney.push(evt.layerY);  
+			 linen.push(1);  
+			 ctx.beginPath();  
+			 ctx.lineWidth = 5 + Math.random() * 10;  
+	
+			 for (var i=1;i<linex.length;i++) {  
+						 lastX = linex[i];  
+						 lastY = liney[i];  
+						 if (linen[i] == 0) {  
+								ctx.moveTo(lastX,lastY);  
+						 } else {  
+								ctx.lineTo(lastX,lastY);  
+						 }  
+			 } 
+			 ctx.stroke();
+		}  
  }  
  function onMouseDown(evt) {  
-    flag = 1;  
-    linex.push(evt.layerX);  
-    liney.push(evt.layerY);  
-    linen.push(0);  
+		flag = 1;  
+		linex.push(evt.layerX);  
+		liney.push(evt.layerY);  
+		linen.push(0);  
  }  
  function onMouseUp(evt) {  
-    flag = 0;  
-     linex.push(evt.layerX);  
-     liney.push(evt.layerY);  
-    linen.push(0);  
+		flag = 0;  
+		 linex.push(evt.layerX);  
+		 liney.push(evt.layerY);  
+		linen.push(0);  
  }  
 
 }
@@ -337,8 +357,14 @@ function blur_drawer(){
 
 
 //放大镜功能
-
-function magnifying_glass(event){	
+var canvas3=document.createElement('canvas');
+	canvas3.style.position='absolute'
+	canvas3.height=200;
+	canvas3.width=200;
+	canvas3.style.top=100;
+	canvas3.style.left=100;
+	item1.appendChild(canvas3);
+	ctx3=canvas3.getContext('2d');
 
 	var ball={
 		x:canvas.width/2,
@@ -357,7 +383,7 @@ function clear(){
 	ctx.clearRect(ball.x-ball.radius,ball.y-ball.radius,ball.radius*2,ball.radius*2)
 }
 
-canvas.addEventListener('mousemove',function(event){
+function mousemove_(event){
 	clear();
 	ball.x=event.layerX;
 	ball.y=event.layerY;
@@ -366,8 +392,16 @@ canvas.addEventListener('mousemove',function(event){
 	var pixinfo = ctx.getImageData(ball.x,ball.y,10,10);
 	ctx3.putImageData(pixinfo,0,0,100,100,200,200);
 	
-})
+
+	
+}
+
+function magnifying_glass(event){	
+
+canvas.addEventListener('mousemove',mousemove_);
+
 canvas.addEventListener('mouseout',function(){
+	
 	ctx.clearRect(0,0,canvas.width,canvas.height);
 	readAsDataURL();
 })
@@ -387,7 +421,20 @@ canvas.addEventListener('mousemove',magnifying_glass);
 }
 
 var mag_glass=document.getElementById('magnifying_glass')
-mag_glass.addEventListener('click',magnifying_glass);
+	var i=1;
+	mag_glass.addEventListener('click',function()
+	{	
+		if(i)
+			{magnifying_glass(event);
+				console.log('fuckyou')
+			i=0;}
+		
+		else if(!i){
+			canvas.removeEventListener('mousemove',mousemove_)
+		}
+	});
+
+
 //下载模块
 
 btn2.addEventListener('click',function(){
@@ -397,28 +444,28 @@ btn2.addEventListener('click',function(){
 //图片下载操作,指定图片类型
 function download(type) {
 
-    //设置保存图片的类型
-    var imgdata = canvas.toDataURL(type);
-    
-    //将mime-type改为image/octet-stream
-    var fixtype = function (type) {
-        type = type.toLocaleLowerCase().replace(/jpg/i, 'jpeg');
-        var r = type.match(/png|jpeg|bmp|gif/)[0];
-        return 'image/' + r;
-    }
-    imgdata = imgdata.replace(fixtype(type), 'image/octet-stream')
-    
-    //将图片保存到本地
-    var saveFile = function (data, filename) {
-        var link = document.createElement('a');
-        link.href = data;
-        link.download = filename;
-        var event = document.createEvent('MouseEvents');
-        event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-        link.dispatchEvent(event);
-       
-        console.log(canvas.width,canvas.height)
-    }
-    var filename = new Date().toLocaleDateString() + '.' + type;
-    saveFile(imgdata, filename);
+		//设置保存图片的类型
+		var imgdata = canvas.toDataURL(type);
+		
+		//将mime-type改为image/octet-stream
+		var fixtype = function (type) {
+				type = type.toLocaleLowerCase().replace(/jpg/i, 'jpeg');
+				var r = type.match(/png|jpeg|bmp|gif/)[0];
+				return 'image/' + r;
+		}
+		imgdata = imgdata.replace(fixtype(type), 'image/octet-stream')
+		
+		//将图片保存到本地
+		var saveFile = function (data, filename) {
+				var link = document.createElement('a');
+				link.href = data;
+				link.download = filename;
+				var event = document.createEvent('MouseEvents');
+				event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+				link.dispatchEvent(event);
+			 
+				console.log(canvas.width,canvas.height)
+		}
+		var filename = new Date().toLocaleDateString() + '.' + type;
+		saveFile(imgdata, filename);
 }
